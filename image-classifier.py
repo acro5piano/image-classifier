d4794b526d0b9b40fc5752b0f8cd00a7b9e6011e
gosho-kazuya <ketsume0211@gmail.com>
2016-04-23 13:10:24 +0900

    Trim unnecessary lines

--- image-classifier.py
+++ image-classifier.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
-
 
 # learninig data
 data_training = np.loadtxt('data/train-images.txt', delimiter=' ')
@@ -13,11 +11,9 @@ data_test = np.loadtxt('data/test-images.txt', delimiter=' ')
 label_test = np.loadtxt('data/test-labels.txt', delimiter=' ')
 
 # learn
-#estimator = LinearSVC(C=1.0)
 estimator = RandomForestClassifier()
 estimator.fit(data_training, label_training)
 
 # test
 print(estimator.score(data_test, label_test))
-
 
